fb64ef7c5990577d004478e175c0e2f5a11f1e39
zhangpengfei <971232132@qq.com>
2024-07-10 08:58:07 +0800

    first commit

---
+++ src/main/kotlin/com/zpf/novademo01/Novademo01Application.kt
@@ -0,0 +1,11 @@
+package com.zpf.novademo01
+
+import org.springframework.boot.SpringBootConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Novademo01Application
+fun main(args: Array<String>) {
+    runApplication<Novademo01Application>(*args)
+}
---
+++ src/test/kotlin/com/zpf/novademo01/Novademo01ApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.zpf.novademo01
+
+import org.junit.jupiter.api.Test
+
+class Novademo01ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
